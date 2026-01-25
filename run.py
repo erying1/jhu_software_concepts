f462986fd43d06deab53db1bf1ce1439fa5d4dba
erying1 <erying1@jh.edu>
2026-01-25 10:30:56 -0800

    Initial commit for Module 1

---
+++ run.py
@@ -0,0 +1,17 @@
+# Modern Concepts in Python: Spring 2026
+# by Eric Rying
+#
+# Module 1 Assignment: Personal Website
+#
+# run.py
+#
+
+from flask import Flask, Blueprint, render_template
+from app.__init__ import start_my_app
+
+app = start_my_app()
+
+# Create the main function and use port= 8080 as per the assignment requirements
+# SHALL run at port 8080 and localhost or 0.0.0.0
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
